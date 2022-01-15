43ad82b1a407b7c9f2d4e664a6d0720e0d4cebad
kiokomay <86175265+kiokomay@users.noreply.github.com>
2022-01-14 17:43:29 -0800

    auth
    added auth in config, auth page, user login, logout, and forms

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from "./logo.svg";
+import React, { Component } from "react";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn my butt
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    user: null,
+  };
+
+  setUserInState = (incomingUserData) => {
+    this.setState({ user: incomingUserData });
+  };
+
+  componentDidMount() {
+    let token = localStorage.getItem("token");
+    if (token) {
+      let userDoc = JSON.parse(atob(token.split(".")[1])).user;
+      this.setState({ user: userDoc });
+    }
+  }
+
+  render() {
+    return (
+      <main className="App">
+        <h1>hello</h1>
+      </main>
+    );
+  }
 }
 
 export default App;
