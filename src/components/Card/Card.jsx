d7058a0ad58cf66807db1ec800ae3c1b03cc06a6
Matt Lobo <mattl99@hotmail.ca>
2022-01-17 00:35:10 -0500

    CSS UPDATE

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./Card.css";
+
+function Card(props) {
+  return (
+    <div className="CardBox">
+      <div className="Card nes-container" id="NoPadding">
+        <div className="CardTitle"> {props.title} </div>
+        <div className="CardProgress">
+          <div className="CardProgressValue">24/25</div>
+          <div className="CardProgressBar">
+            <progress
+              class="nes-progress is-primary"
+              id="BarHeight"
+              value="80"
+              max="100"
+            ></progress>
+          </div>
+        </div>
+        <div className="CardBtns" id="NoPadding">
+            <button className="nes-btn CardBtn">View Expenses</button>
+            <button className="nes-btn CardBtn">View Expenses</button>
+            
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
