fd9088a25ddd7ddd0809195335b8a48612fe3f99
Mahmoud El-Shawa <moshawa0@gmail.com>
2022-01-17 21:18:50 -0500

    working basic model

--- models/user.js
+++ models/user.js
@@ -1,11 +1,26 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const expenseSchema = new Schema({
+  amount: Number,
+  detail: String,
+  date: {
+    type: Date,
+    default: () => Date.now(),
+  }
+})
+
+const categorySchema = new Schema({
+  name: String,
+  max: Number,
+  expenses: [expenseSchema]
+})
+
 const userSchema = new Schema({
-  name: {type: String, required: true},
+  name: { type: String, required: true },
   email: {
     type: String,
-    // unique: true,
+    unique: true,
     trim: true,
     lowercase: true,
     required: true
@@ -13,17 +28,26 @@ const userSchema = new Schema({
   password: {
     type: String,
     trim: true,
-    // minLength: 8,
+    minLength: 8,
     required: true
-  }
+  },
+  budget: {
+    total: Number,
+    categories: [categorySchema]
+  },
+  exp: Number,
+  img: String
 }, {
   timestamps: true,
   toJSON: {
-    transform: function(doc, ret) {
+    transform: function (doc, ret) {
       delete ret.password;
       return ret;
     }
   }
 });
+
+
+
 
 module.exports = mongoose.model('User', userSchema);
