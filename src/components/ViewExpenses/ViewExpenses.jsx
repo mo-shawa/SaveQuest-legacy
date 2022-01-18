1bd510d2870068548d005ff9425c7bfa86370258
Matt Lobo <mattl99@hotmail.ca>
2022-01-18 14:02:49 -0500

    Modals completed

--- src/components/ViewExpenses/ViewExpenses.jsx
+++ src/components/ViewExpenses/ViewExpenses.jsx
@@ -4,20 +4,32 @@ import "./ViewExpenses.css";
 function ViewExpenseModal(props) {
   return (
     <div className={props.isModalOpen ? "modal-bg bg-active" : "modal-bg"}>
-      <div className="SmallModal">
-        <table>
-          <tbody>
-            <tr>
-              <td></td>
-            </tr>
-          </tbody>
-        </table>
-        <button className="submit" type="submit">
-          Submit
-        </button>
+      <div className="SmallModal nes-container">
+      <h2 className="ModalHeader">{props.category} Expenses</h2>
         <div className="modal-close">
           <span onClick={() => props.viewModalOpen(false)}>X</span>
         </div>
+        <div className="AllExpenseWrapper" id="ViewExpenseWrapper">
+        <table>
+                  <tr>
+                  <th>Item</th>
+                  <th>Date</th>
+                  <th>Amount</th>
+                  </tr>
+                  <tr>
+                      <td>Anime Pillow</td>
+                      <td>Yesterday</td>
+                      <td>200$</td>
+                  </tr>
+                  <tr>
+                      <td>Anime Pillow</td>
+                      <td>Yesterday</td>
+                      <td>200$</td>
+                  </tr>
+              </table>
+          
+        </div>
+
       </div>
     </div>
   );
