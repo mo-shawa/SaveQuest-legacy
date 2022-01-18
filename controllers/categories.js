18647218e0467b9d92971b69ab06ef91037d13a5
Mahmoud El-Shawa <moshawa0@gmail.com>
2022-01-18 17:59:00 -0500

    updates to controllers

--- controllers/categories.js
+++ controllers/categories.js
@@ -3,19 +3,26 @@ const { CategoryModel } = require('../models/user')
 
 
 const createCat = async (req, res) => {
-    // console.log('Req body: ', req.body)
-    const user = await UserModel.findById(req.params.user_id)
-    const newCat = new CategoryModel(req.body)
-    // console.log(newCat)
-    user.budget.categories.push(newCat)
-    user.save()
-    res.status(200).json(user)
+    try {
+        // console.log('Req body: ', req.body)
+        const user = await UserModel.findById(req.params.user_id)
+        const newCat = new CategoryModel(req.body)
+        // console.log(newCat)
+        user.budget.categories.push(newCat)
+        user.save()
+        res.status(200).json(user)
+    } catch (error) {
+        res.status(400).json(error.message)
+    }
+
 
 }
 
 const deleteCat = async (req, res) => {
     try {
-        CategoryModel.findByIdAndDelete(req.params.cat_id)
+        const deleted = CategoryModel.findByIdAndDelete(req.params.cat_id)
+        console.log(deleted)
+        res.status(200).json('success')
 
     } catch (error) {
         console.log(error.message)
