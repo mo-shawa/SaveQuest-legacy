840bc3c1d21fd589fa77cd96f10c51a0ffff41df
Mahmoud El-Shawa <moshawa0@gmail.com>
2022-01-17 21:22:09 -0500

    bandaid env issue

--- config/database.js
+++ config/database.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.DATABASE_URL, {
+mongoose.connect(process.env.DATABASE_URL || 'mongodb://127.0.0.1:27017/savequest', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
 
 const db = mongoose.connection
 
-db.on('connected', () =>{
+db.on('connected', () => {
     console.log(`Mongoose connected to ${db.name} at ${db.host}:${db.port}`)
 })
 
