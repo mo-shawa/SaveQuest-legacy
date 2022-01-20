8f247188277c0d44eb2ee0efe2849cccefa3b79e
Matt Lobo <mattl99@hotmail.ca>
2022-01-19 20:15:09 -0500

    GARBAGE CANS IN

--- src/components/ManageBudgetModal/ManageBudgetModal.jsx
+++ src/components/ManageBudgetModal/ManageBudgetModal.jsx
@@ -4,24 +4,24 @@ import { Doughnut } from "react-chartjs-2";
 import "chart.js/auto";
 
 function ManageBudgetModal(props) {
-
   const handleDelete = async (cat_id) => {
     try {
-      const fetchResponse = await fetch(`/api/users/${props.user._id}/categories/${cat_id}`, {
-        method: "DELETE",
-        headers: { "Content-Type": "application/json" },
-      })
+      const fetchResponse = await fetch(
+        `/api/users/${props.user._id}/categories/${cat_id}`,
+        {
+          method: "DELETE",
+          headers: { "Content-Type": "application/json" },
+        }
+      );
 
-      if (!fetchResponse.ok) throw new Error("Delete failed!")
+      if (!fetchResponse.ok) throw new Error("Delete failed!");
 
-      const userData = await fetchResponse.json()
-      props.setUserInState(userData)
+      const userData = await fetchResponse.json();
+      props.setUserInState(userData);
     } catch (error) {
-      console.log(error.message)
-
+      console.log(error.message);
     }
-  }
-
+  };
 
   return (
     <div className={props.isModalOpen ? "modal-bg bg-active" : "modal-bg"}>
@@ -33,18 +33,40 @@ function ManageBudgetModal(props) {
         <div className="ModalDivider">
           <div className="ModaLinksWrapper">
             <ul>
-              <button className="nes-btn" onClick={() => props.createModalOpen(true)}>New Item +</button>
+              <button
+                className="nes-btn"
+                onClick={() => props.createModalOpen(true)}
+              >
+                New Item +
+              </button>
               <li>Edit Budgets</li>
               <hr />
 
-              {props.user.budget.categories.map(cat => {
-                return <>
-                  <span onClick={() => handleDelete(cat._id)}>DELETE - </span><li onClick={() => props.editModalOpen(true, cat.name)} catId={cat._id} className="CatLinks">{cat.name}</li>
-
-
-                </>
+              {props.user.budget.categories.map((cat) => {
+                return (
+                  <div className="listItems">
+                    <span className="Trash" onClick={() => handleDelete(cat._id)}>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="20"
+                        height="20"
+                        fill="currentColor"
+                        class="bi bi-trash-fill"
+                        viewBox="0 0 16 16"
+                      >
+                        <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z" />
+                      </svg>{" "}
+                    </span>
+                    <li
+                      onClick={() => props.editModalOpen(true, cat.name)}
+                      catId={cat._id}
+                      className="CatLinks"
+                    >
+                    {cat.name}
+                    </li>
+                  </div>
+                );
               })}
-
             </ul>
           </div>
           <div className="PiChart">
