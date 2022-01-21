806d4e8aad2ea32a615ceb103c6ab10717c91197
Mahmoud El-Shawa <moshawa0@gmail.com>
2022-01-21 02:03:51 -0500

    basic level system on expense

--- src/components/Avatar/Avatar.jsx
+++ src/components/Avatar/Avatar.jsx
@@ -3,9 +3,37 @@ import "./Avatar.css"
 
 class Avatar extends Component {
   state = {
+    experience: this.props.exp,
     level: 1,
     nextLevelExp: 100,
     img: 'Images/female5.png'
+  }
+
+  componentDidMount() {
+    if (this.state.experience > this.state.nextLevelExp) {
+
+      this.setState((state) => {
+        return {
+          ...state,
+          experience: (this.state.experience - this.state.nextLevelExp),
+          level: state.level + 1,
+          nextLevelExp: state.nextLevelExp * 2,
+          img: 'Images/female7.png'
+        }
+      })
+    }
+    if (this.props.exp < 100) {
+      this.setState((state) => {
+        return {
+          ...state,
+          level: 1,
+          nextLevelExp: 100,
+          img: 'Images/female5.png'
+        }
+      })
+    }
+
+
   }
   render() {
     return (
@@ -16,21 +44,21 @@ class Avatar extends Component {
               <h2>{this.props.user.name}</h2>
               <div className="innerCircle">
                 <img className="Avatar"
-                  src="Images/female5.png"
+                  src={this.state.img}
                   alt="gril"
                 />
               </div>
               <div className="AvatarLevel">
-                <h2 className="LevelValue">Level: 3 </h2>
+                <h2 className="LevelValue">Level: {this.state.level}</h2>
               </div>
               <div className="AvatarProgressBar">
                 <progress
                   class="nes-progress is-success AvatarProgress"
                   id="BarHeight"
-                  value={this.props.exp}
-                  max="2400"
+                  value={this.state.experience}
+                  max={this.state.nextLevelExp}
                 ></progress>
-                <h5>{this.props.exp}/2400xp</h5>
+                <h5>{this.state.experience}/{this.state.nextLevelExp}xp</h5>
               </div>
             </div>
           </div>
