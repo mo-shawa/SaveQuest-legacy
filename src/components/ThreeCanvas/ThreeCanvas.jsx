88030c2af6b8f0953e83e205c8603a8600c6cab5
Mahmoud El-Shawa <moshawa0@gmail.com>
2022-04-27 19:04:53 -0400

    remove skybox, add static background

--- src/components/ThreeCanvas/ThreeCanvas.jsx
+++ src/components/ThreeCanvas/ThreeCanvas.jsx
@@ -1,30 +1,15 @@
 import React, { Suspense } from "react"
-import { Canvas, useThree } from '@react-three/fiber';
+import { Canvas } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
 import Dollar from "../Dollar/Dollar";
 import { useNavigate } from "react-router-dom";
-import { CubeTextureLoader } from 'three'
 
-function SkyBox() {
-    const { scene } = useThree();
-    const loader = new CubeTextureLoader();
-    const texture = loader.load([
-        '/skybox//river_walk_1/pix/posx.png', // front
-        '/skybox//river_walk_1/pix/negx.png', // back
-        '/skybox//river_walk_1/pix/posy.png', // up
-        '/skybox//river_walk_1/pix/negy.png', // down 
-        '/skybox//river_walk_1/pix/posz.png', // right
-        '/skybox//river_walk_1/pix/negz.png', // left
-    ])
-    scene.background = texture;
-    return null
-}
 
 export default function ThreeCanvas(props) {
     const navigate = useNavigate()
     return (
         <Canvas camera={{ position: [100, 0, 0] }} >
-            <SkyBox />
+            <color attach="background" args={["#747cc5"]} />
             <ambientLight intensity={0.3} />
             <spotLight position={[10, 10, 10]} />
             <Suspense fallback={null}>
