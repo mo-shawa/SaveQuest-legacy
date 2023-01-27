064526185ff6fcc0c188100c83eef630bf35ab69
moshawa <moshawa0@gmail.com>
2023-01-26 23:59:03 -0500

    landing accessible while logged in

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,26 +1,32 @@
 import "./Header.css"
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from "react-router-dom"
 
-function Header({user, userLogout}) {
-        return (
-            <div className='header'>
-                <div className='Filler'>
-                </div>
-                <div className='Title'>
-                    <Link to={'app'}>
-                        <img className='TitleImg' src="/Images/cooltext402280913904255.png" alt="Save Quest" />
+function Header({ user, userLogout }) {
+	const { pathname } = useLocation()
+	console.log(location)
+	return (
+		<div className="header">
+			<div className="Filler"></div>
+			<div className="Title">
+				<Link to={pathname === "/app" ? "/" : "/app"}>
+					<img
+						className="TitleImg"
+						src="/Images/cooltext402280913904255.png"
+						alt="Save Quest"
+					/>
+				</Link>
+			</div>
+			<div className="LoginWrapper" id="HeaderLogin">
+				{user ? (
+					<button className="nes-btn" onClick={() => userLogout()}>
+						LOGOUT
+					</button>
+				) : (
+					""
+				)}
+			</div>
+		</div>
+	)
+}
 
-                    </Link>
-                </div>
-                <div className='LoginWrapper' id='HeaderLogin'>
-                    {user ?
-                        <button className="nes-btn" onClick={() => userLogout()}>LOGOUT</button>
-                        : ''
-                    }
-                </div>
-            </div>
-        )
-    }
-
-
-export default Header;
+export default Header
